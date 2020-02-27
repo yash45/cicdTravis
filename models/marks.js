95f87d48e1618e2d6208dad1d25e6a750bb3da25
yash45 <lebronash@gmail.com>
2020-02-27 16:45:24 +0300

    deploying project using travis

---
+++ models/marks.js
@@ -0,0 +1,42 @@
+class Marks{
+    constructor(subject, mark, student){
+        this.subject = subject;
+        this.mark = mark;
+        this.student = student;
+    }
+    isValid(){
+        let valid = true;
+        this.student === null? 'please enter a student name' : true;
+        typeof(this.mark !== 'number')? true: 'please enter a number'; 
+        return valid
+    }
+    grade(){
+        let score = null;
+        switch(true){
+            case mark < 50:
+                 score = 'F';
+                 break;
+            case mark >= 50 && mark <60:
+                score = 'E'
+                break;
+            case mark >= 60 && mark < 70:
+                score = 'D'
+                break;
+            case mark >= 70 && mark < 80:
+                score = 'C'
+                break;
+            case mark >= 50 && mark < 90:
+                score = 'B'
+                break;
+            case mark >=90 && mark <=100:
+                score = 'A'
+                break;
+            default: 
+                score = 'enter grade'
+                 break;
+        }
+        return score
+    }
+}
+
+module.exports = Marks;
---
+++ models/student.js
@@ -0,0 +1,12 @@
+/** @format */
+
+class Student {
+  constructor(firstName, lastName, age, status) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.status = status;
+  }
+}
+
+module.exports = Student
